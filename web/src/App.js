a60423c08ee39478fe830a2df425ad1a3f359f2a
matheus <matheusbritodasilva@gmail.com>
2020-01-16 18:06:30 -0300

    form parcialmente

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+//Component:  Bloco isolado de Html, css e js , o qual nao interfere no restante da aplicacao
+//Propriedade: Informacoes que um componente PAI passa para o componente FILHO
+//Estado:
+import "./global.css";
+import "./Sidebar.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="username_github">Usuário o Github</label>
+            <input name="github_username" id="username_github" required></input>
+          </div>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+          <div class="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button>Salvar</button>
+        </form>
+      </aside>
+      <main></main>
     </div>
   );
 }
